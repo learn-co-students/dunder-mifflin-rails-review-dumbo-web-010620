8543b5be1279956edbfacaeeb2835ef3d86d4063
wenglin1 <wenglin@buffalo.edu>
2020-02-07 01:57:19 -0500

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :first_name, :last_name, :title, :office, :img_url, presence: true
+    validates :alias, :title, uniqueness: true
+    validates :img_url, format: URI::regexp(%w[http https])
 end
